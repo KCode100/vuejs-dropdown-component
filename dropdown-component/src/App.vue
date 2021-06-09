0a709d074ae264fe0b659541756b9f5af680d672
Kivi Corn <akiva.corn@cyber-duck.co.uk>
2021-06-09 17:25:55 +0300

    upload files

---
+++ dropdown-component/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="app">
+		<header>
+			<Navbar />
+		</header>
+	</div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+export default {
+	name: 'App',
+	components: {
+		Navbar,
+	},
+};
+</script>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+body {
+	font-family: 'monstserrat', sans-serif;
+}
+header {
+	width: 100vw;
+	background-color: #222;
+	padding: 15px;
+}
+</style>
